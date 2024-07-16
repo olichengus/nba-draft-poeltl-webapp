258b202f2ca40fb22a8283e9c82e370da83cfc2f
olicheng <olicheng@student.ubc.ca>
2024-07-16 14:26:56 -0700

    Handled player selected logic

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@
 import './App.css';
 import React from 'react';
 import { useEffect, useState } from 'react';
-import {Button, Grid, GridItem, HStack} from "@chakra-ui/react";
-import {ReactNode} from "react";
+import {Button, GridItem, HStack, Table, Thead} from "@chakra-ui/react";
 import {
     AutoComplete,
     AutoCompleteInput,
@@ -19,6 +18,7 @@ function App() {
   const [poeltlPlayer, setPoeltlPlayer] = useState("");
   const [guessedPlayer, setGuessedPlayer] = useState("");
   const [hint, setHint] = useState("");
+  const [selected, setSelected] = useState("");                                                                                                     
   const [guesses, setGuesses] = useState([]);
   const [gameFinished, setGameFinished] = useState(false);
   const [options, setOptions] = useState([]);
@@ -36,32 +36,31 @@ function App() {
   }
 
   function handleGuess(){
-      console.log("Handling Guess");
-      console.log(guessedPlayer);
-      console.log(poeltlPlayer);
-      fetch('/api/guess_player', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-              player_name: guessedPlayer,
-              poeltl_name: poeltlPlayer
-          })
-        }).then(
-          res => res.json().then(
-              data=>{
-                  console.log("data fetched");
-                  const guess = data.data;
-                  const result = data.result;
-                  if (typeof guess === 'string'){
-                      return ;
-                  } else {
-                      setGuesses(guesses => [...guesses, guess]);
-                      result === 'wrong' ? setGameFinished(false): setGameFinished(true)
-                  }
-              }
-          ))
+    if (selected !== ""){
+        fetch('/api/guess_player', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                player_name: guessedPlayer,
+                poeltl_name: poeltlPlayer
+            })
+          }).then(
+            res => res.json().then(
+                data=>{
+                    console.log("data fetched");
+                    const guess = data.data;
+                    const result = data.result;
+                    if (typeof guess === 'string'){
+                        return ;
+                    } else {
+                        setGuesses(guesses => [...guesses, guess]);
+                        result === 'wrong' ? setGameFinished(false): setGameFinished(true)
+                    }
+                }
+            ));
+    }
   }
 
   useEffect(()=> {
@@ -86,13 +85,6 @@ function App() {
         }
 
 
-  function renderGuesses(): ReactNode{
-      return(
-          <GridItem>{JSON.stringify(guesses)}</GridItem>
-      );
-  }
-
-
   return (
     <div className="App">
       <header className="App-header">
@@ -100,9 +92,11 @@ function App() {
           Poeltl player is {poeltlPlayer} from {hint}
         </p>
           <HStack>
-              <AutoComplete color="black" openOnFocus>
-                  <AutoCompleteInput variant="filled" />
-                  <AutoCompleteList>
+              <AutoComplete color="black" openOnFocus onSelectOption={(params) => {
+                console.log(params);
+                setSelected(params.item.value)}}>
+                  <AutoCompleteInput color="black" variant="filled" onChange={(event) => {setGuessedPlayer(event.target.value)}} />
+                  <AutoCompleteList color="black">
                       {options.map((option) => (
                           <AutoCompleteItem
                               key={option}
@@ -114,12 +108,20 @@ function App() {
                       ))}
                   </AutoCompleteList>
               </AutoComplete>
-              <Button>Guess</Button>
+              <Button onClick={handleGuess}>Guess</Button>
           </HStack>
       </header>
-        <Grid color='blue'>
-            {renderGuesses()}
-        </Grid>
+        <div className='App-body' style={{marginTop:0}}>
+            <Table>
+                <Thead>
+                    <tr>
+                        <th>Player</th>
+                        <th>College</th>
+                        <th>Result</th>
+                    </tr>
+                </Thead>
+            </Table>
+        </div>
     </div>
   );
 }
