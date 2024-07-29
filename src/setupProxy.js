961f1ab6bb76b7291c903054b160ee3f7d10e0cb
olicheng <olicheng@student.ubc.ca>
2024-07-28 19:59:08 -0700

    Changed to productuion serer

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:8080',
+            target: 'https://draft-poeltl-backend-production.up.railway.app',
             changeOrigin: false,
         })
     );
